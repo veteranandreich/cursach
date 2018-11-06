690ff80ce8e55344a1ad14b8466a106aba1e0eee
Vladimir <vladimir27goncharov@icloud.com>
2018-11-06 14:05:55 +0300

    Добавлена таблица проектов и детальная информация об участниках

--- cursach/main/urls.py
+++ cursach/main/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Kino', views.steps, name='steps'),
+    path('Kino/persons', views.persons, name='persons'),
+    path('DomainChecker', views.steps, name='steps'),
+    path('DomainChecker/persons', views.persons, name='persons'),
+    #Сделать автодописывание двух новых строчек проекта
 
 ]
--- cursach/main/views.py
+++ cursach/main/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+import json
+
+with open('main/projects.json', 'r', encoding='utf-8') as fh:
+    maininfo = json.load(fh)
+
+with open('main/persons.json', 'r', encoding='utf-8') as fx:
+    personsinfo = json.load(fx)
+
+
 
 def index(request):
-    return render(request, 'main/mainpage.html')
+    return render(request, 'main/mainpage.html', {'x': maininfo})
+
+def persons(request):
+    a = str(request)
+    a = a[20:-2]
+    lastsl = a.index('/')
+    a = a[:lastsl]
+    return render(request, 'main/persons.html', {'x': personsinfo, 'sitename': a})
+
+def steps(request):
+    pass
+    '''return render(request, 'main/steps.html', {'x': data})'''
