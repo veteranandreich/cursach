bbf6aa256dd48bac1882d56233e6bb4378fedb63
Vladimir <vladimir27goncharov@icloud.com>
2018-11-07 16:05:19 +0300

    Охапка дров - курсач готов

--- cursach/main/urls.py
+++ cursach/main/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('auth', views.auth, name='auth'),
     path('Kino', views.steps, name='steps'),
     path('Kino/persons', views.persons, name='persons'),
     path('DomainChecker', views.steps, name='steps'),
--- cursach/main/views.py
+++ cursach/main/views.py
@@ -7,10 +7,13 @@ with open('main/projects.json', 'r', encoding='utf-8') as fh:
 with open('main/persons.json', 'r', encoding='utf-8') as fx:
     personsinfo = json.load(fx)
 
+with open('main/steps.json', 'r', encoding='utf-8') as fr:
+    stepsinfo = json.load(fr)
 
 
 def index(request):
     return render(request, 'main/mainpage.html', {'x': maininfo})
+
 
 def persons(request):
     a = str(request)
@@ -19,6 +22,30 @@ def persons(request):
     a = a[:lastsl]
     return render(request, 'main/persons.html', {'x': personsinfo, 'sitename': a})
 
+
 def steps(request):
-    pass
-    '''return render(request, 'main/steps.html', {'x': data})'''
+    a = str(request)
+    a = a[20:-2]
+    return render(request, 'main/steps.html', {'x': stepsinfo, 'sitename': a})
+
+
+def login(request):
+    return render(request, 'main/loginpage.html')
+
+
+def auth(request):
+    print(request.POST.get('login'), "        CHECHNYA        ", request.POST.get('password'))
+    logininfo = [
+        {"login": "admin",
+         "password": "admin"
+         },
+
+        {"login": "manager",
+         "password": "manager"
+         }
+    ]
+    if request.POST:
+        for i in logininfo:
+            if i["login"] == request.POST.get("login") and i["password"] == request.POST.get("password"):
+                return render(request, 'main/adminpage.html')
+    return render(request, 'main/mainpage.html', {'x': maininfo})
