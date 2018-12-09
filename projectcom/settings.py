a4df85153433d75417b0109b47c90000644b31c3
Vladimir <vladimir27goncharov@icloud.com>
2018-12-09 17:54:10 +0300

    Update 4.0

--- projectcom/settings.py
+++ projectcom/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cursach.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cursach.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
